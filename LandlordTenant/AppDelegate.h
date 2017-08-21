aef4a63bd9660c1ed1c4e44f58c9f4ced0d4d8ee
Tye Blackie <blackie.tye@gmail.com>
2017-08-21 14:06:38 -0700

    Initial Commit

---
+++ LandlordTenant/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  LandlordTenant
+//
+//  Created by Tye Blackie on 2017-08-21.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ LandlordTenant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LandlordTenant
+//
+//  Created by Tye Blackie on 2017-08-21.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
